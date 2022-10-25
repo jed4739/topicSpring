56770077f9a224ff7b8e90ff04f761e1b1c8b64d
jed4739 <tjsdlr04@gmail.com>
2022-10-25 14:51:02 +0900

    second

---
+++ src/main/java/com/example/topicSpring/model/User.java
@@ -0,0 +1,48 @@
+package com.example.topicSpring.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+@Entity
+@Table(name = "author")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(length = 100, name = "userid", unique = true)
+    private String userid;
+
+    @Column(length = 100, name = "mail", unique = true)
+    private String mail;
+
+    @Column(length = 100, name = "userPassword")
+    private String userPassword;
+
+    @Column(length = 15, name = "name")
+    private String name;
+
+    @Column(length = 100, name = "profile")
+    private String profile;
+
+    @Column(length = 20, name = "phone")
+    private String phone;
+
+    @Column(name = "created")
+    private String created;
+
+    private String role;
+
+    public void updateName(String name) {
+        this.name = name;
+    }
+}
