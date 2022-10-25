41795452b4a0a7bead310167cb86d5e3b67ad06d
jed4739 <tjsdlr04@gmail.com>
2022-10-25 12:53:01 +0900

    first

---
+++ src/main/java/com/example/topicSpring/TopicSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.topicSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopicSpringApplication.class, args);
+	}
+
+}
