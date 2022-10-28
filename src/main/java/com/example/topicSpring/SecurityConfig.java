4899281acdbeb5032413311ed93f3b97832c1c3d
jed4739 <tjsdlr04@gmail.com>
2022-10-28 13:09:43 +0900

    test

--- src/main/java/com/example/topicSpring/SecurityConfig.java
+++ src/main/java/com/example/topicSpring/SecurityConfig.java
@@ -1,11 +1,15 @@
 package com.example.topicSpring;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -14,29 +18,27 @@ import org.springframework.security.web.SecurityFilterChain;
 @Slf4j
 @EnableWebSecurity
 @Configuration
+@AllArgsConstructor
 public class SecurityConfig {
 
-        @Bean
-        public InMemoryUserDetailsManager userDetailsService() {
-            String password = passwordEncoder().encode("1111");
-            InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-            manager.createUser(User.withUsername("user")
-                    .password(password)
-                    .roles("USER")
-                    .build());
-            manager.createUser(User.withUsername("admin")
-                    .password(password)
-                    .roles("ADMIN")
-                    .build());
-            return manager;
-        }
+    private final UserDetailsService userDetailsService;
 
+    /** 권한 설정 */
+
+
+    /** img,css,js security setting */
     @Bean
-    // Service 에서 비밀번호를 암호화할 수 있도록 Bean 으로 등록
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    /** Service 에서 비밀번호를 암호화할 수 있도록 Bean 으로 등록 */
+    @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    /** url roles 접근 제한 */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
