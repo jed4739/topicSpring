bea02e17ba8d020563663818401b8326d459882e
jed4739 <tjsdlr04@gmail.com>
2022-11-10 13:16:02 +0900

    조금수정

---
+++ src/main/java/com/example/topicSpring/model/Board.java
@@ -0,0 +1,26 @@
+package com.example.topicSpring.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Board extends BaseTime{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @OneToMany(mappedBy = "board", cascade = CascadeType.REMOVE)
+    private List<Comment> commentList;
+
+}
---
+++ src/main/java/com/example/topicSpring/model/Comment.java
@@ -0,0 +1,21 @@
+package com.example.topicSpring.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Comment extends BaseTime{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne
+    private Board board;
+}
