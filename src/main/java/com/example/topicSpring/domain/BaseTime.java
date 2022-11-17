9592cc5f591853d0d7c010eb7a24ad78ce0835eb
jed4739 <tjsdlr04@gmail.com>
2022-11-17 14:27:45 +0900

    ModifiedDate add annotation change

--- src/main/java/com/example/topicSpring/domain/BaseTime.java
+++ src/main/java/com/example/topicSpring/domain/BaseTime.java
@@ -2,13 +2,14 @@ package com.example.topicSpring.domain;
 
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
-import java.time.ZonedDateTime;
 
 /**
  * BaseTime 은 엔티티에 생성날짜를 추가해줍니다.
@@ -18,7 +19,14 @@ import java.time.ZonedDateTime;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTime {
+
     @CreatedDate
-    @Column(nullable = false, name = "createdDate", updatable = false)
+    @Column(nullable = false, updatable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd/HH:mm:ss")
     private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd/HH:mm:ss")
+    private LocalDateTime modifiedDate;
 }
