4b3250a9a88264415c1118a5e70d8d61a8c0e40f
jed4739 <tjsdlr04@gmail.com>
2022-11-24 19:17:41 +0900

    ㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠ

---
+++ src/main/java/com/example/topicSpring/auth/PrincipalDetailService.java
@@ -0,0 +1,21 @@
+package com.example.topicSpring.auth;
+
+import com.example.topicSpring.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PrincipalDetailService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        return null;
+    }
+}
